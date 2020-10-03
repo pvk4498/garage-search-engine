8c39ee43cb945dcbff25e2eb7cca10e9f259a951
pvk4498 <70468969+pvk4498@users.noreply.github.com>
2020-10-03 19:50:33 +0530

    Add files via upload

---
+++ css/search_helper.js
@@ -0,0 +1,54 @@
+function get_json_Data(url,callback){
+
+  var result;
+  $.ajax({
+    
+    url:url,
+    success:callback
+  });
+  return result;
+}
+
+var url='https://pvk4498.github.io/garage-search-engine/json/data.json';
+var data = get_json_Data(url,handleResult);
+
+function handleResult(result){
+  console.log(result);
+  
+
+   var options={
+        valueNames:[
+                    'id', 
+                    'name',
+                    'address'
+                    ,'city'
+                    ,'contact',
+                    { name: 'image', attr: 'src' },
+                    { name: 'link', attr: 'href' }
+                  ],
+        item:'myitem',
+        page: 10,
+        pagination: true
+      };
+     
+   var userList = new List('mylist',options,result);
+   
+   userList.on('updated', function (list) {
+		if (list.matchingItems.length > 0) {
+			$('.no-result').hide()
+		} else {
+			$('.no-result').show()
+		}
+	});
+   
+
+   $('#clear-btn').click(function() {
+    $('#search').val('');
+    userList.search();
+ });
+
+ 
+ 
+}
+
+
